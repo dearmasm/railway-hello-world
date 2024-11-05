f852dc7c06a49c08ec69e38374eba28e702fe16e
Mario De Armas <dearmasm@yahoo.com>
2024-11-05 10:50:22 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import pg from "pg";
+
 const express = require('express');
 const app = express();
 
@@ -6,6 +8,14 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     return res.send('Hello World from Railway using a Db!');
 });
+
+const pool = new pg.Pool();
+
+app.get('/jokes', async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM jokes")
+
+  res.json(rows)
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
