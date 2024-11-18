b7a10d0750b4950f6bc8df6a5c7d1c97cf7edf33
Mario De Armas <dearmasm@yahoo.com>
2024-11-17 20:41:42 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.get("/posts", async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    console.log(connectionString);
+    console.log(process.env.DATABASE_URL);
 });
